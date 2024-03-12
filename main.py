c886cf51806c2c04942cb7e322f0cf46e6d33d66
Mizhik <vlad.smilono2018@gmail.com>
2024-03-12 21:51:19 +0200

    add style.kv - like css

--- main.py
+++ main.py
@@ -1,48 +1,26 @@
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
 
-import requests
-import json
+Builder.load_file("style.kv")
 
-API="aab1fb0cd6f961b2d87c23ae2c01ab64"
+class MyGridLayout(Widget):
+    name = ObjectProperty(None)
+    surname = ObjectProperty(None)
+    age = ObjectProperty(None)
 
-class MyGridLayout(GridLayout):
-    def __init__(self, **kwargs):
-        super(MyGridLayout,self).__init__(**kwargs)
+    def press(self):
+        name = self.name.text
+        surname = self.surname.text
+        age = self.age.text
 
-        self.cols = 1
-
-        self.top_grid = GridLayout()
-        self.top_grid.cols = 2
-
-        self.top_grid.add_widget(Label(text="City: "))
-        self.city = TextInput(multiline=False)
-        self.top_grid.add_widget(self.city)
-
-        self.add_widget(self.top_grid)
-
-        self.submit = Button(text = "Ok", font_size = 32)
-        self.submit.bind(on_press = self.press)
-        self.add_widget(self.submit)
-
-    def press(self, instance):
-        city = self.city.text
-        res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric')
-
-        if res:
-            data = json.loads(res.text)
-            temp = data['main']['temp']
-            result = Label(text=f'Weather in {city} is {temp}')
-        else:
-            result = Label(text="Erorr city")
-
-        self.add_widget(result)
-        self.city.text = ''
-
+        #self.add_widget(Label(text=f'Hello {surname} {name}, your age is {age}'))
+        print(f'Hello {surname} {name}, your age is {age}')
+        self.name.text = ''
+        self.surname.text = ''
+        self.age.text = ''
 
 class MyApp(App):
     def build(self):
